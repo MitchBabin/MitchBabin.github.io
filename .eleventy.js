699fde607e15a9d18047349947a1dd5bc41c9aa7
Knealand <knealandy@gmail.com>
2020-03-18 09:01:38 -0400

    Small Fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,19 @@
 module.exports = function(eleventyConfig) {
+
+    // eleventyConfig.addCollection("doc", (collection) =>
+    //     collection.getFilteredByGlob("documentation/*.md").sort((a, b) => {
+    //         return b.position - a.position;
+    //     })
+    // );
+
+    eleventyConfig.addCollection("doc", function(collection) {
+        var temp = collection.getFilteredByTag("documentation");
+        return temp.sort((a,b) => {
+            return a.data.position - b.data.position;
+        })
+    });
+
+
     eleventyConfig.addPassthroughCopy('Images');
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('js');
